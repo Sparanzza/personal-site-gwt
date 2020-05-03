9f597c3c7a7ed8a26930e5b065bd4bf3be80e43c
Aurelio Muñoz <aurelio@sparanzza.com>
2020-05-03 10:38:32 +0200

    web base with reactivity

---
+++ website-client/src/main/java/com/sparanzza/application/EmptyPresenter.java
@@ -0,0 +1,26 @@
+package com.sparanzza.application;
+
+import com.google.gwt.user.client.ui.HTML;
+import com.intendia.reactivity.client.CompositeView;
+import com.intendia.reactivity.client.Place;
+import com.intendia.reactivity.client.PresenterChild;
+import com.sparanzza.NameTokens;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+
+public class EmptyPresenter extends PresenterChild<EmptyPresenter.MyView> {
+
+    public static @Singleton class MyPlace extends Place {
+        @Inject MyPlace(Provider<EmptyPresenter> p) {
+            super(NameTokens.emptyPage, asSingle(p));
+        }
+    }
+
+    public static class MyView extends CompositeView {
+        @Inject MyView() { initWidget(new HTML("<div style='text-align: center; font-size: 120px;'>😬</div>")); }
+    }
+
+    @Inject EmptyPresenter(MyView view, ApplicationPresenter.MainContent parent) { super(view, parent); }
+}
